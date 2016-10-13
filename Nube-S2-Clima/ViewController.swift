bdddc40d43931342f5a9f93c931b2bae0fc2b78a
hansellesteban <hansell.ramos@gmail.com>
2016-10-12 21:19:43 -0500

    Initial Commit

---
+++ Nube-S2-Clima/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nube-S2-Clima
+//
+//  Created by Hansel Ramos Osorio on 10/12/16.
+//  Copyright © 2016 Hansel Ramos Osorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
